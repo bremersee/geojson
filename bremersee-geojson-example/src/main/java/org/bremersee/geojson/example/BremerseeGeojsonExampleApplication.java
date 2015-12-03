8b28e3b37dcbe1fda1b4d2f2d9f6fa7d64be6447
Christian Bremer <christian.bremersee.org>
2015-12-03 11:22:12 +0100

    bremersee-geojson-example init

---
+++ bremersee-geojson-example/src/main/java/org/bremersee/geojson/example/BremerseeGeojsonExampleApplication.java
@@ -0,0 +1,12 @@
+package org.bremersee.geojson.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BremerseeGeojsonExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BremerseeGeojsonExampleApplication.class, args);
+    }
+}
---
+++ bremersee-geojson-example/src/test/java/org/bremersee/geojson/example/BremerseeGeojsonExampleApplicationTests.java
@@ -0,0 +1,18 @@
+package org.bremersee.geojson.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BremerseeGeojsonExampleApplication.class)
+@WebAppConfiguration
+public class BremerseeGeojsonExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
